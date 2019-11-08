dc017cadcf830a6f17f397a4f05599e3eb4e1591
Josh-Hollopeter <joshuahollopeter@gmail.com>
2019-11-08 16:58:46 -0700

    Added Scanner

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,12 +1,13 @@
+import java.util.Scanner;
 
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
 		// TODO: add pressButton() method
 		// TODO: add turnON() method
+		Scanner input = new Scanner(System.in);
 
+		input.close();
 	}
-	
-	
 
 }

27168e6c60c2c650279564e63899b0d49090d6ac
Josh-Hollopeter <joshuahollopeter@gmail.com>
2019-11-08 17:13:10 -0700

    initial turnON method completed

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -6,8 +6,19 @@ public class RemoteControlApp {
 		// TODO: add pressButton() method
 		// TODO: add turnON() method
 		Scanner input = new Scanner(System.in);
+		String remotePower = "";
+		do {
+			System.out.println("To begin, turn on the remote control type ON to power remote");
+			remotePower = input.next();
 
+		} while (!remotePower.equals("ON"));
 		input.close();
-	}
 
+		if (remotePower.equals("ON")) {
+			turnON();
+		}
+	}
+	public static void turnON() {
+		System.out.println("TURNING ON...");
+	}
 }

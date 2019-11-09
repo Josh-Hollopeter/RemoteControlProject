5dc9b1c2af1ec310ff457f253b9042b18dc77cb5
Josh-Hollopeter <joshuahollopeter@gmail.com>
2019-11-08 18:05:41 -0700

    Working but needs cleaned up

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -3,22 +3,80 @@ import java.util.Scanner;
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
-		// TODO: add pressButton() method
-		// TODO: add turnON() method
+
 		Scanner input = new Scanner(System.in);
 		String remotePower = "";
+
 		do {
 			System.out.println("To begin, turn on the remote control type ON to power remote");
 			remotePower = input.next();
 
 		} while (!remotePower.equals("ON"));
-		input.close();
 
-		if (remotePower.equals("ON")) {
-			turnON();
+		while (!remotePower.equals("OFF")) {
+			System.out.println("Enter a button 0 - 9 enter OFF to power down");
+			remotePower = input.next();
+			if (remotePower.equals("ON")) {
+				turnON();
+				remotePower = "loop";
+				continue;
+			} else if (remotePower.equals("OFF")) {
+				turnOff();
+				break;
+
+			} else {
+				switch (remotePower) {
+					case "0":
+						pressButton();
+						continue;
+					case "1":
+						pressButton();
+						continue;
+					case "2":
+						pressButton();
+						continue;
+					case "3":
+						pressButton();
+						continue;
+					case "4":
+						pressButton();
+						continue;
+					case "5":
+						pressButton();
+						continue;
+					case "6":
+						pressButton();
+						continue;
+					case "7":
+						pressButton();
+						continue;
+					case "8":
+						pressButton();
+						continue;
+					case "9":
+						pressButton();
+						continue;
+					default:
+						System.out.println("Comand not recognized");
+				}
+
+			}
 		}
+		input.close();
 	}
+
 	public static void turnON() {
 		System.out.println("TURNING ON...");
+
 	}
+
+	public static void pressButton() {
+		System.out.println("Boop");
+
+	}
+
+	public static void turnOff() {
+		System.out.println("TURNING OFF...");
+	}
+
 }

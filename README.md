2e608ba57628c97ef70f82529b1cad905ccb5a42
Josh-Hollopeter <joshuahollopeter@gmail.com>
2019-11-08 20:26:47 -0700

    cleaned up code and updated README

--- README.md
+++ README.md
@@ -4,17 +4,35 @@
 
 #### Overview
 This is a remote control simulation.
+It's designed to allow a user to input commands
+to simulate button presses on a remote
 
 #### Concepts
-* loops
+* while loops
 * switch
 * if statements
-* expression
+* statements
+* methods
+* boolean
+* do while loops
+* importing packages
+* Scanner
+* break
+* continue
 
 #### Technologies Used
 * java 
-* while loops
+* git
+* shell
+* markdown
+* eclipse IDE
+* atom
+
 
 #### Lessons Learned
 * commit more often!
 * keep it simple
+
+#### Program Operation
+*Follow the onscreen prompts.
+*Input is case sensitive where applicable.
--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -8,17 +8,16 @@ public class RemoteControlApp {
 		String remotePower = "";
 
 		do {
-			System.out.println("To begin, turn on the remote control type ON to power remote");
+			System.out.println("To begin, turn on the remote control: Type ON to power remote");
 			remotePower = input.next();
 
 		} while (!remotePower.equals("ON"));
 
-		while (!remotePower.equals("OFF")) {
+		while (true) {
 			System.out.println("Enter a button 0 - 9 enter OFF to power down");
 			remotePower = input.next();
 			if (remotePower.equals("ON")) {
 				turnON();
-				remotePower = "loop";
 				continue;
 			} else if (remotePower.equals("OFF")) {
 				turnOff();
@@ -27,32 +26,14 @@ public class RemoteControlApp {
 			} else {
 				switch (remotePower) {
 					case "0":
-						pressButton();
-						continue;
 					case "1":
-						pressButton();
-						continue;
 					case "2":
-						pressButton();
-						continue;
 					case "3":
-						pressButton();
-						continue;
 					case "4":
-						pressButton();
-						continue;
 					case "5":
-						pressButton();
-						continue;
 					case "6":
-						pressButton();
-						continue;
 					case "7":
-						pressButton();
-						continue;
 					case "8":
-						pressButton();
-						continue;
 					case "9":
 						pressButton();
 						continue;
@@ -71,7 +52,7 @@ public class RemoteControlApp {
 	}
 
 	public static void pressButton() {
-		System.out.println("Boop");
+		System.out.println("Boop!");
 
 	}
 
